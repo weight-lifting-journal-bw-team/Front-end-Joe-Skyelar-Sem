1432c727a2e6aff1925b372a02c38212c82b0260
Sem8 <mssemmi8@gmail.com>
2019-03-11 13:35:05 -0400

    sem edits

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
+
 class Home extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Home page</h1>
+				<h3>Add Workout</h3>
+				<h3>Welcome, ${User}</h3>
+				<h2>Dashboard</h2>
+				<h2>Stats</h2>
+				<h2>Chart</h2>
 			</div>
 		);
 	}
