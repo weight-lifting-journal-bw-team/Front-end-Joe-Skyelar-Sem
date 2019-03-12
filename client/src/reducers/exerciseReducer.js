bd551d1757144e7056968e3d0c8aea46205e2107
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-12 16:25:03 -0700

    Created adsExercise reducer

---
+++ client/src/reducers/exerciseReducer.js
@@ -0,0 +1,39 @@
+import {
+	ADD_EXERCISE_START,
+	ADD_EXERCISE_SUCCESS,
+	ADD_EXERCISE_FAILURE
+} from "../actions";
+
+const initialState = {
+	exercises: [],
+	fetching: false,
+	errors: null
+};
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case ADD_EXERCISE_START:
+			return {
+				...state,
+				fetching: true,
+				errors: null
+			};
+
+		case ADD_EXERCISE_SUCCESS:
+			return {
+				...state,
+				exercises: [...state.exercises, action.payload],
+				fetching: false,
+				errors: null
+			};
+
+		case ADD_EXERCISE_SUCCESS:
+			return {
+				...state,
+				fetching: false,
+				errors: action.payload
+			};
+		default:
+			return state;
+	}
+};
