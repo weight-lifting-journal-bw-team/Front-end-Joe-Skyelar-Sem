67cd151b6228bb31485c0ee80228940b6400814c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-12 19:01:14 -0700

    Completed Exercise Component delete event handler

--- client/src/components/Workouts/Workout.js
+++ client/src/components/Workouts/Workout.js
@@ -1,44 +1,55 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { deleteExercise } from "../../actions/exerciseActions";
 
 class Workout extends Component {
-  state = {
-    workoutToggle: false
-  }
+	state = {
+		workoutToggle: false
+	};
 
-  toggleWorkout = () => {
-      this.setState({
-          workoutToggle: !this.state.workoutToggle
-      })
-  }
+	toggleWorkout = () => {
+		this.setState({
+			workoutToggle: !this.state.workoutToggle
+		});
+	};
 
-  render() {
-    return (
-      <Fragment
-      >
-        <div   
-        onClick={this.toggleWorkout} 
-        className="workout-container">
-            Click this to expand this workout v
-            <h1>{this.props.workout_name}</h1>
-            {this.state.workoutToggle && <div
-            className="expanded-content"
-            >  
-                <img src={this.props.progress_picture}/>
-                <p>{this.props.current_weight}</p>
-                <p>{this.props.max_weight}</p>
-                <p>{this.props.workout_sets}</p>
-                <p>{this.props.workout_reps}</p>
-                <p>{this.props.body_region}</p>
-                <p>{this.props.workout_distance}</p>
-                <p>{this.props.workout_notes}</p>
-                <p>{this.props.workout_time}</p>
-                <p>{this.props.workout_type}</p>
-                {/* Add Exercise Form */}
-            </div>}
-        </div>
-      </Fragment>
-    )
-  }
+	deleteExercise = e => {
+		e.preventDefault();
+
+		this.props.deleteExercise(this.props.workout_id);
+	};
+
+	render() {
+		return (
+			<Fragment>
+				<div onClick={this.toggleWorkout} className="workout-container">
+					Click this to expand this workout v
+					<h1>{this.props.workout_name}</h1>
+					{this.state.workoutToggle && (
+						<div className="expanded-content">
+							<img src={this.props.progress_picture} />
+							<p>{this.props.current_weight}</p>
+							<p>{this.props.max_weight}</p>
+							<p>{this.props.workout_sets}</p>
+							<p>{this.props.workout_reps}</p>
+							<p>{this.props.body_region}</p>
+							<p>{this.props.workout_distance}</p>
+							<p>{this.props.workout_notes}</p>
+							<p>{this.props.workout_time}</p>
+							<p>{this.props.workout_type}</p>
+
+							<button onClick={this.deleteExercise}>
+								Delete
+							</button>
+						</div>
+					)}
+				</div>
+			</Fragment>
+		);
+	}
 }
 
-export default Workout;
+export default connect(
+	null,
+	{ deleteExercise }
+)(Workout);
